63a59b9b2d44381e601415aaa3109a0d3a07e2fb
ADMIN <puruvora824@gmail.com>
2023-12-22 23:50:23 -0800

    This is my First Commit

---
+++ app/src/main/java/com/example/stopwatchapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.stopwatchapplication;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView stopwatchDisplay;
+    private Button startButton, stopButton, resetButton;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+    private long timeInMilliseconds = 0L;
+    private long timeSwapBuff = 0L;
+    private long updatedTime = 0L;
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = System.currentTimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            stopwatchDisplay.setText("" + mins + ":" + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        stopwatchDisplay = findViewById(R.id.stopwatchDisplay);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startTime = System.currentTimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwapBuff = 0L;
+                customHandler.removeCallbacks(updateTimerThread);
+                stopwatchDisplay.setText("00:00:00");
+            }
+        });
+    }
+}
